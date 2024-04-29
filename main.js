51d53a639e33593224c63dcec2761deb92f721a0
estefaniemily <128614741+estefaniemily@users.noreply.github.com>
2024-04-29 08:07:35 -0300

    m

--- main.js
+++ main.js
@@ -40,12 +40,3 @@ let dias=Math.floor (horas/24);
 segundos %=60;
 minutos %=60;
 horas %=24;
-
-return dias+" dias "+ horas+" horas "+minutos+" minutos "+segundos+" segundos ";}
-if (tempoFinal>0){
-    return dias+" dias "+ horas+" horas "+minutos+" minutos "+segundos+" segundos ";
-    }
-    else {
-        return "PRAZO FINALIZADO";
-    }
-    
